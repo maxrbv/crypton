3b2b3b6729e174c4cdb6d5d939a123f12e30ecf3
maxrbv <maxkekqwe@gmail.com>
2022-05-03 18:44:42 +0500

    fix transactions

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,19 +1,18 @@
-import { useState, useMemo } from 'react';
-import { createInstance } from 'react-async';
-import { Button, AppBar, Box, Toolbar, Typography, TextField} from '@mui/material';
+import React, { useState, useMemo } from 'react';
 import { Connection, Transaction, clusterApiUrl, PublicKey } from '@solana/web3.js';
 import * as anchor from '@project-serum/anchor';
 import BN from "bn.js"
 import provider from './lib/AnchorProvider';
 import anchorProgram from './lib/program';
 import { donationId, ownerId } from './lib/pubKey'
-import donor from './accounts/donor.json' // huita
+import fund from './accounts/fund.json'
 import Wallet from '@project-serum/sol-wallet-adapter';
-import styles from './styles'
+
+import { Button, AppBar, Box, Toolbar, Typography, TextField}  from '@mui/material';
 
 
 function App() {
-  anchor.setProvider(provider)
+  anchor.setProvider(provider);
 
   const network = clusterApiUrl('devnet');
   const providerUrl = 'https://www.sollet.io';
@@ -24,110 +23,132 @@ function App() {
   const [donationAmount, setDonationAmount] = useState<string>('');
   const [owner, setOwner] = useState<PublicKey>(PublicKey.default);
 
+  const [balance, setBalance] = useState<number>(0);
+
   const isOwner = () => owner.toString() === ownerId.toString();
 
   wallet.on('connect', () => {
-      setConnected(true);
-      if (wallet.publicKey?.toString() === ownerId.toString())
-          setOwner(ownerId);
-      console.log('Connected:' + wallet.publicKey?.toBase58());
+    setConnected(true);
+    if (wallet.publicKey?.toString() === ownerId.toString()) {
+      setOwner(ownerId);
+      showBalance(donationId);
+    }
+    else
+      showBalance(wallet.publicKey as PublicKey)
+    console.log('Connected: ' + wallet.publicKey?.toBase58());
   });
 
   wallet.on('disconnect', () => {
-      setConnected(false);
-      setOwner(PublicKey.default);
-      console.log('Disconnected');
+    setConnected(false);
+    setOwner(PublicKey.default);
+    console.log('Disconnected');
   });
   
-  const getBalance = async() => {
-      let balance : number = 0;
-      if (wallet.publicKey){
-          balance = await connection.getBalance(wallet.publicKey);
-          return (balance / 1000000000).toFixed(3);
-      }
+  const showBalance = async(pubKey: PublicKey) => {
+    setBalance(await connection.getBalance(pubKey) / Math.pow(10, 9))
   }
-  const AsyncBalance = createInstance({promiseFn:getBalance},'AsyncBalance');
 
-  const sendLamports = async(from: PublicKey, to: PublicKey , amount: number) => {
-    console.log('Im here')
+  const prepareTransaction = async(from: PublicKey, to: PublicKey , amount: number) => {
     let transaction = new Transaction();
-    console.log('Next 1')
     transaction.feePayer = from;
     transaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash
+    const fee = await connection.getFeeForMessage(
+      transaction.compileMessage(),
+      'confirmed',
+    );
 
-    if (!isOwner()) {
-      console.log('ne owner', amount)
-      const tx = await anchorProgram.instruction.transfer(new BN(amount * 1000000000), {
-        accounts: {
-          from,
-          to,
-          systemProgram: anchor.web3.SystemProgram.programId
+    if (isOwner()) {
+      const tx  = await anchorProgram.instruction.transfer(
+        new BN(amount - fee.value),
+        {
+          accounts: {
+            from: from,
+            to: to,
+            systemProgram: anchor.web3.SystemProgram.programId
+        }
+      });
+      transaction.add(tx);
+      anchor.web3.sendAndConfirmTransaction(connection, transaction, [anchor.web3.Keypair.fromSecretKey(new Uint8Array(fund as Array<number>))]);
+      console.log("Transaction confirmed")
+    }
+    else {
+      const tx = await anchorProgram.instruction.transfer(
+        new BN(amount),
+        {
+          accounts: {
+            from: from,
+            to: to,
+            systemProgram: anchor.web3.SystemProgram.programId
         },     
       } as any);
-      console.log(tx)
       transaction.add(tx);
       transaction = await wallet.signTransaction(transaction);
       let signature = await anchor.web3.sendAndConfirmRawTransaction(connection, transaction.serialize());
-      console.log("Submitted transaction " + signature + ", awaiting confirmation")
-      await connection.confirmTransaction(signature)
-      console.log("Transaction " + signature + " confirmed")
-    }
-    else {
-      const tx = await anchorProgram.instruction.transfer(new BN(amount), {
-        accounts: {
-          from, to,
-          systemProgram: anchor.web3.SystemProgram.programId
-        }
-      })
-
-      console.log(tx);
-      transaction.add(tx);
-      anchor.web3.sendAndConfirmTransaction(connection, transaction, [anchor.web3.Keypair.fromSecretKey(new Uint8Array(donor as Array<number>))]);
+      console.log("Transaction signed " + signature);
+      await connection.confirmTransaction(signature);
+      console.log("Transaction " + signature + " confirmed");
     }
   }
   
-  const onFinish = async() => {
+  const sendDonation = async() => {
     if (isOwner()) {
-      console.log('start')
-      await sendLamports(donationId, ownerId, Number(donationAmount) * Math.pow(10, 9));
-      console.log(`send ${donationAmount} lamport to ${ownerId.toBase58()}`);
+      const fundBalance = await connection.getBalance(donationId);
+      console.log('Preparing transaction')
+      await prepareTransaction(donationId, ownerId, fundBalance);
+      await showBalance(donationId);
+      console.log(`Withdrawed ${fundBalance} lamports to ${ownerId.toBase58()}`);
     }
     else {
-      console.log('not owner sending')
-      console.log(wallet.publicKey, donationId, Number(donationAmount) * Math.pow(10, 9))
-      await sendLamports(wallet.publicKey as PublicKey, donationId, Number(donationAmount));
-      console.log(`send ${donationAmount} lamport from ${wallet.publicKey?.toBase58()}`);
+      console.log('Preparing transaction')
+      await prepareTransaction(wallet.publicKey as PublicKey, donationId, Number(donationAmount));
+      await showBalance(wallet.publicKey as PublicKey);
+      console.log(`Sent ${donationAmount} lamports from ${wallet.publicKey?.toBase58()}`);
     }
   }
   
   return (
-      <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-          <Toolbar>
-          <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
-              DONATION
-          </Typography>
-          {connected ?
-          <p>{wallet.publicKey?.toBase58() ?? 'Unable to get data'}</p>
-          :
-          <Button style={styles.BtnConnect} variant="contained" onClick={() => { wallet.connect()} }>Connect Wallet</Button>
-          }
-          </Toolbar>
-      </AppBar>
+    <Box sx={{ flexGrow: 1 }}>
+    <AppBar position="static">
+      <Toolbar>
+      <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+        DONATION
+      </Typography>
       {connected ? (
-      <div style={{display:'flex',justifyContent:'center',alignItems:'center',margin:'auto',height:'80vh',flexDirection:'column'}}>
-          <div style={{display:'flex'}}>
-              <TextField label="Amount" value={donationAmount} onChange={e => setDonationAmount(e.target.value)}/>
-              <Button onClick={onFinish}>Make a donation</Button>
-          </div>
-          <AsyncBalance>
-              <AsyncBalance.Fulfilled>
-                  {balance => <p>Your balance: {balance} SOL</p>}
-              </AsyncBalance.Fulfilled>
-          </AsyncBalance>
+      <Button style={{height:'5vh',width:'30vh',fontSize:'20px'}} variant="contained" color="error" onClick={() => { wallet.disconnect()} }>Disconnect</Button>
+      ) :
+      <Button style={{height:'5vh',width:'30vh',fontSize:'20px'}} variant="contained" color="success" onClick={() => { wallet.connect()} }>Connect Wallet</Button>
+      }
+      </Toolbar>
+    </AppBar>
+    {connected ? (
+    <div id='main' style={{display:'flex',justifyContent:'center',alignItems:'center',margin:'auto',height:'10vh',flexDirection:'column',paddingTop:'60px'}}>
+      {isOwner() ? (
+      <><div id='wallet'>
+        <p style={{fontSize:'32px'}}>Account({wallet.publicKey?.toBase58() ?? 'Unable to get data'})</p>
+        <p style={{fontSize:'20px'}}>Curent fund balance: {balance} SOL</p>
       </div>
-      ) : null }
-      </Box>
+      <div id='donation' style={{display:'flex'}}>
+        <Button onClick={sendDonation} style={{paddingLeft:'20px'}}>Withdraw donation</Button>
+      </div>
+      <div id='update'>
+        <Button onClick={() => showBalance(donationId)} style={{marginTop:'20px'}}>Update balance</Button>
+      </div></>
+      ) : (
+      <><div id='wallet'>
+        <p style={{fontSize:'32px'}}>Account({wallet.publicKey?.toBase58() ?? 'Unable to get data'})</p>
+        <p style={{fontSize:'20px'}}>Curent balance: {balance} SOL</p>
+      </div>
+      <div id='donation' style={{display:'flex'}}>
+        <TextField label="Amount (lamports)" value={donationAmount} onChange={e => setDonationAmount(e.target.value)}/>
+        <Button onClick={sendDonation} style={{paddingLeft:'20px'}}>Make a donation</Button>
+      </div>
+      <div id='update'>
+        <Button onClick={() => showBalance(wallet.publicKey as PublicKey)} style={{paddingTop:'20px'}}>Update balance</Button>
+      </div></>
+      )}
+    </div>
+    ) : null }
+    </Box>
   );
 }
 
